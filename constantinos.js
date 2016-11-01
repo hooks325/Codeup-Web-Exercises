65af033438eceabbaf01e4ab1ec57396111ba961
nedwaldie <nw_waldie3@yahoo.com>
2016-11-01 09:41:31 -0500

    My first commit

---
+++ constantinos.js
@@ -0,0 +1,19 @@
+/**
+ * Created by nedwaldie on 10/28/16.
+ */
+$(document).ready(function() {
+    $("#intro").slideDown(1500);
+    $("#slogan").slideDown(1500);
+    // $( "#pizzaPanel" ).click(function() {
+    //     $( "#pizza" ).slideToggle("slow");
+    // });
+    // $( "#wingPanel" ).click(function() {
+    //     $( "#wings" ).slideToggle("slow");
+    // });
+    // $( "#pastaPanel" ).click(function() {
+    //     $( "#pastas" ).slideToggle("slow");
+    // });
+    // $( "#sandwhichPanel" ).click(function() {
+    //     $( "#sandwhiches" ).slideToggle("slow");
+    // });
+});
