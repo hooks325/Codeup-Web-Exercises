00d866d39670c3f49cfc3df4f58ac1dd6db227e8
nedwaldie <nw_waldie3@yahoo.com>
2016-11-30 14:24:45 -0600

    animate_exercises

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -4,7 +4,45 @@
 
 
 $(document).ready(function() {
+
+    // shows the 10 facts when the a tag is clicked
     $('#answers').click(function() {
-        $('dd').toggleClass('invisible');
+        $('dd').fadeToggle(1000);
+    });
+
+    // bolds the first of each li
+    $('.national-parks').each(function(){
+        $(this).children().first().css('font-weight', 'bold');
+    });
+
+    // when you click a li it hides all li of that parent
+    $('li').click(function() {
+        $(this).parent().hide();
+    });
+
+    // Shows li items when click 'toggle' button
+    $('#toggle').click(function() {
+        $('ul').slideDown();
+    });
+
+    // when Big Bend is clicked toggles the facts
+    $('#big-bend').click(function() {
+        $('#big-bend-facts').slideToggle();
+    });
+
+    // when Alibates Flint Quarries is clicked toggles the facts
+    $('#alibates').click(function() {
+        $('#alibates-facts').slideToggle();
+    });
+
+    // when San Antonio Missions is clicked toggles the facts
+    $('#missions').click(function() {
+        $('#missions-facts').slideToggle();
+    });
+
+    $('left').click(function() {
+        $('#move-box').animate({
+            right: '100px'
+        }, 1000);
     })
 });
